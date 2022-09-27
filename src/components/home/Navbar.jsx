17017b83b9c9fdd7bde0387752a69dc7a93ef818
samueleffa <seffa1@gmail.com>
2022-09-27 07:26:52 -0400

    trying to get sign out modal to open on its parent

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -7,21 +7,46 @@ function Navbar() {
   // Sign Out Modal
   const [signOutModalIsOpen, setSignOutModalIsOpen] = React.useState(false);
 
+  // Profile position
+
+  const [profilePosition, setProfilePosition] = React.useState(0);
+
+  function getPosition() {
+    let rect = document.getElementById("profile");
+    if (rect) setProfilePosition(rect.x);
+    return 0;
+  }
+
+  // getPosition();
+
   return (
     <>
-      {/* Modals */}
-
+      {/* Sign Out Modals */}
       <Modal
+        disablePortal
         className="Modal"
         isOpen={signOutModalIsOpen}
         contentLabel="SignOut Modal"
         onRequestClose={() => setSignOutModalIsOpen(false)}
-        style={{ overlay: { background: "none" } }}
+        style={{
+          overlay: { background: "none" },
+          content: {
+            position: "absolute",
+            // bottom: "100px",
+            right: `0px`,
+            width: "300px",
+            height: "200px",
+            background: "black",
+            borderRadius: "20px",
+            boxShadow: "1px 0px 6px 2px rgba(255, 255, 255, 0.3)",
+          },
+        }}
+        parentSelector={() => document.getElementById("navbar")}
       >
         <SignOut />
       </Modal>
 
-      <nav className="Navbar">
+      <nav className="Navbar" id="navbar">
         <ul className="Navbar__ul">
           <li className="Navbar__ul__li">
             <i class="fa-brands fa-twitter"></i>
@@ -65,6 +90,7 @@ function Navbar() {
         <article
           className="Navbar__profile"
           onClick={() => setSignOutModalIsOpen(true)}
+          id="profile"
         >
           <img src={profilePic} className="profile-img--small"></img>
           <div className="Navbar__profile__name-container">
